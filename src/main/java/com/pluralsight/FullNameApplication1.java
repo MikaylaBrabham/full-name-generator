e866f52d4ee009bbda33abcfd7ada1139c6fcdaf
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-26 03:24:27 -0500

    entered last and first name

--- src/main/java/com/pluralsight/FullNameApplication1.java
+++ src/main/java/com/pluralsight/FullNameApplication1.java
@@ -8,25 +8,49 @@ public class FullNameApplication1 {
 
         // scanner for user to use
         Scanner scanner = new Scanner(System.in);
+        // enter the defined methods
+
+
     }
-    public String fullName(Scanner scanner) {
+    public static String getName(Scanner scanner) {
         // ask the user for their first name (required)
         // display their first name
         System.out.println("Enter your first name: ");
-        String firstName = scanner.nextLine();
+        String firstName = scanner.nextLine().trim();
+
+        // make sure their is imput and trailing and leading statements are clear
+        while (firstName.isEmpty()) {
+            System.out.println("Please enter your first name, it is required: ");
+            firstName = scanner.nextLine().trim();
+        }
+
 
         // ask user for their middle name (optional)
         // display their middle name or require them to press enter
         System.out.println("Enter your middle name: ");
-        String middleName= scanner.nextLine();
+        String middleName = scanner.nextLine();
+
 
         // ask user for their last name (required)
         // display their last name
         // ensure they can't leave blank and prompt them to enetr again
+        System.out.println("Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        // make sure their is imput and trailing and leading statements are clear
+        while (lastName.isEmpty()) {
+            System.out.println("Please enter your first name, it is required: ");
+            lastName = scanner.nextLine().trim();
+        }
+
+
 
         // ask user for their suffic (optional)
         // display their suffix or require them to press enter
+        System.out.println("Enter your suffix: ");
+        String suffixName = scanner.nextLine();
 
         // make sure all have trimmed leading and triling spaces
+
     }
 }
