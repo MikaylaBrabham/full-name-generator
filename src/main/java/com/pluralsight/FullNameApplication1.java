31aec831543bb68abf5cceb039dc5b0936b151ac
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-26 18:26:08 -0500

    finished constructing fbut for some reason its sill printing double

--- src/main/java/com/pluralsight/FullNameApplication1.java
+++ src/main/java/com/pluralsight/FullNameApplication1.java
@@ -9,6 +9,11 @@ public class FullNameApplication1 {
         // scanner for user to use
         Scanner scanner = new Scanner(System.in);
         // enter the defined methods
+         String fullName = getName(scanner);
+        System.out.println("Full Name: " + fullName);
+         // close the scanner
+         scanner.close();
+
 
 
     }
@@ -24,33 +29,49 @@ public class FullNameApplication1 {
             firstName = scanner.nextLine().trim();
         }
 
-
         // ask user for their middle name (optional)
         // display their middle name or require them to press enter
         System.out.println("Enter your middle name: ");
-        String middleName = scanner.nextLine();
+        String middleName = scanner.nextLine().trim();
 
 
         // ask user for their last name (required)
         // display their last name
         // ensure they can't leave blank and prompt them to enetr again
         System.out.println("Enter your last name: ");
-        String lastName = scanner.nextLine();
+        String lastName = scanner.nextLine().trim();
 
         // make sure their is imput and trailing and leading statements are clear
         while (lastName.isEmpty()) {
-            System.out.println("Please enter your first name, it is required: ");
+            System.out.println("Please enter your last name, it is required: ");
             lastName = scanner.nextLine().trim();
         }
 
-
-
-        // ask user for their suffic (optional)
+        // ask user for their suffix (optional)
         // display their suffix or require them to press enter
         System.out.println("Enter your suffix: ");
-        String suffixName = scanner.nextLine();
+        String suffixName = scanner.nextLine().trim();
 
         // make sure all have trimmed leading and triling spaces
-
+        //String fullName = firstName + "|" + middleName + "|" + lastName + "|" + suffixName;
+        //String [] nameParts = fullName.split("\\|");
+       String fullName = firstName + " " + middleName + " " + lastName + ", " + suffixName;
+        if (middleName.isEmpty() && suffixName.isEmpty()){
+            String noSuffixMiddleName = firstName + " " + lastName;
+            System.out.println("Full Name: " + noSuffixMiddleName);
+        }
+        if (suffixName.isEmpty()){
+            String noSuffix = firstName + " " + middleName + " " + lastName;
+            System.out.println("Full Name: " + "," + noSuffix);
+        }
+        if (middleName.isEmpty()){
+            String noMiddle = firstName + " " + lastName + ", " + suffixName;
+            System.out.println("Full Name: " + noMiddle);
+        }
+        //else {
+         // String fullName = firstName + " " + middleName + " " + lastName + " ," + suffixName;
+         //   System.out.println("Full Name: " + fullName);
+        //}
+        return fullName;
     }
 }
